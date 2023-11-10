71404caaa675bac296e1081b6b73b3a52f9f03e8
FusionPolaris <carl@polarisdesigned.com>
2023-11-10 23:39:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,44 @@ const redButton = document.getElementById('redButton');
 const yellowButton = document.getElementById('yellowButton');
 const captureButton = document.getElementById('capture');
 
+// Function to change color
+function changeColor(color) {
+  context.drawImage(video, 0, 0, canvas.width, canvas.height);
+  let frame = context.getImageData(0, 0, canvas.width, canvas.height);
+  let data = frame.data;
+
+  for (let i = 0; i < data.length; i += 4) {
+    let red = data[i];
+    let green = data[i + 1];
+    let blue = data[i + 2];
+
+    if (color === 'blue') {
+      // Example logic: If the pixel is blue, change it to red
+      if (blue > 150 && red < 100 && green < 100) {
+        data[i] = 255;     // Red
+        data[i + 1] = 0;   // Green
+        data[i + 2] = 0;   // Blue
+      }
+    } else if (color === 'red') {
+      // Example logic: If the pixel is red, change it to yellow
+      if (red > 150 && blue < 100 && green < 100) {
+        data[i] = 255;     // Red
+        data[i + 1] = 255; // Green
+        data[i + 2] = 0;   // Blue
+      }
+    } else if (color === 'yellow') {
+      // Example logic: If the pixel is yellow, change it to green
+      if (red > 150 && green > 150 && blue < 100) {
+        data[i] = 0;       // Red
+        data[i + 1] = 255; // Green
+        data[i + 2] = 0;   // Blue
+      }
+    }
+  }
+
+  context.putImageData(frame, 0, 0);
+}
+
 // Access the camera
 function startVideo() {
   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
