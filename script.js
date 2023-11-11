2efd1e5092504ca678df435342264b1ca58addcc
FusionPolaris <carl@polarisdesigned.com>
2023-11-11 00:01:10 +0000

    Update script.js

--- script.js
+++ script.js
@@ -7,73 +7,85 @@ const redButton = document.getElementById('redButton');
 const yellowButton = document.getElementById('yellowButton');
 const captureButton = document.getElementById('capture');
 
-// Function to change color
-function changeColor(color) {
-  context.drawImage(video, 0, 0, canvas.width, canvas.height);
-  let frame = context.getImageData(0, 0, canvas.width, canvas.height);
-  let data = frame.data;
+let currentColor = 'normal'; // Default state, no color change
 
-  for (let i = 0; i < data.length; i += 4) {
-    let red = data[i];
-    let green = data[i + 1];
-    let blue = data[i + 2];
-
-    if (color === 'blue') {
-      // Example logic: If the pixel is blue, change it to red
-      if (blue > 150 && red < 100 && green < 100) {
-        data[i] = 255;     // Red
-        data[i + 1] = 0;   // Green
-        data[i + 2] = 0;   // Blue
-      }
-    } else if (color === 'red') {
-      // Example logic: If the pixel is red, change it to yellow
-      if (red > 150 && blue < 100 && green < 100) {
-        data[i] = 255;     // Red
-        data[i + 1] = 255; // Green
-        data[i + 2] = 0;   // Blue
-      }
-    } else if (color === 'yellow') {
-      // Example logic: If the pixel is yellow, change it to green
-      if (red > 150 && green > 150 && blue < 100) {
-        data[i] = 0;       // Red
-        data[i + 1] = 255; // Green
-        data[i + 2] = 0;   // Blue
-      }
+// Function to process each frame of the video
+function processFrame() {
+    if (video.paused || video.ended) {
+        return;
     }
-  }
 
-  context.putImageData(frame, 0, 0);
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    let frame = context.getImageData(0, 0, canvas.width, canvas.height);
+    let data = frame.data;
+
+    // Apply color change based on the current selected color
+    for (let i = 0; i < data.length; i += 4) {
+        let red = data[i];
+        let green = data[i + 1];
+        let blue = data[i + 2];
+
+        switch(currentColor) {
+            case 'blue':
+                // Change blue pixels to red, then yellow, green, and purple on subsequent clicks
+                if (blue > 100 && red < 100 && green < 100) {
+                    data[i] = 255;     // Red
+                    data[i + 1] = 0;   // Green
+                    data[i + 2] = 0;   // Blue
+                }
+                break;
+            case 'red':
+                // Change red pixels to yellow, green, purple, and blue on subsequent clicks
+                if (red > 100 && blue < 100 && green < 100) {
+                    data[i] = 255;     // Red
+                    data[i + 1] = 255; // Green
+                    data[i + 2] = 0;   // Blue
+                }
+                break;
+            case 'yellow':
+                // Change yellow pixels to green, purple, blue, and red on subsequent clicks
+                if (red > 100 && green > 100 && blue < 100) {
+                    data[i] = 0;       // Red
+                    data[i + 1] = 255; // Green
+                    data[i + 2] = 0;   // Blue
+                }
+                break;
+        }
+    }
+
+    context.putImageData(frame, 0, 0);
+    requestAnimationFrame(processFrame); // Continuously process the next frame
 }
 
-// Access the camera
-function startVideo() {
-  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then(function(stream) {
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch(function(err) {
-        console.error("Error accessing the camera: ", err);
-      });
-  } else {
-    alert("Sorry, your browser does not support video capture.");
-  }
-}
+// Start video processing when the video plays
+video.addEventListener('play', () => {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    processFrame();
+});
 
-// Event listeners for buttons
-blueButton.addEventListener('click', () => changeColor('blue'));
-redButton.addEventListener('click', () => changeColor('red'));
-yellowButton.addEventListener('click', () => changeColor('yellow'));
+// Button click handlers to change the currentColor
+blueButton.addEventListener('click', () => currentColor = 'blue');
+redButton.addEventListener('click', () => currentColor = 'red');
+yellowButton.addEventListener('click', () => currentColor = 'yellow');
 
 // Capture button functionality
 captureButton.addEventListener('click', () => {
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  context.drawImage(video, 0, 0, canvas.width, canvas.height);
-  const imageData = canvas.toDataURL('image/png');
-  // This image data can be used to save the photo to the device.
+    const imageData = canvas.toDataURL('image/png');
+    // Code to save imageData to the device
 });
+
+// Access the camera
+function startVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(stream => {
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch(err => {
+            console.error("Error accessing the camera: ", err);
+        });
+}
 
 // Start the video feed when the window loads
 window.addEventListener('load', startVideo);
